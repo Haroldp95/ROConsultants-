518f0c785bf56502478507a7a49f33475006c1bd
Harold <Haroldpdeboer@gmail.com>
2021-11-23 12:54:09 +0100

    Design Change & Some Functionality.

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,25 @@
-console.log("Working...")
+//? Click event listener
+object.addEventListener("click", toHome);
+
+//? Open home page.
+function toHome() {
+    console.log("Going Home...")
+    location.replace("./index.html")
+}
+
+//? Open Login Screen.
+function toLogin() {
+
+    //Clear innertext
+    document.getElementById("login").innerText = "";
+
+    //Add Icon and new text.
+    var tag = document.createElement("i");
+    tag.className = "fas fa-user-alt";
+    tag.innerText = " Uitloggen"
+    var element = document.getElementById("login");
+    element.appendChild(tag);
+    
+    //Change Class.
+    document.getElementById("login").className = "login";
+}
