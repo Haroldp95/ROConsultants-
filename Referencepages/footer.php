7f965fe420b69037ace2d3f3bab621680c81f4b7
ArendKijkindeVegte <78079513+ArendKijkindeVegte@users.noreply.github.com>
2021-12-07 11:08:52 +0100

    Opmaak login.

--- Referencepages/footer.php
+++ Referencepages/footer.php
@@ -1,72 +1,27 @@
-   <!-- ? Footer -->
-    <div class="bar">
-
+<div class="footer">
+      <footer>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-1">
+              <h3 class="headF">Links</h3>
+              <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>Over Ons</li>
+              </ul>
+            </div>
+            <div class="col-md-2">
+              <h3 class="headF">Collega's</h3>
+              <ul>
+                <li>Daan Smets</li>
+                <li>Harold de Boer</li>
+                <li>Arend Kijkt in de Vegte</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
-
-    <div class="container-fluid footerDiv">
-      <div class="row">
-        <div class="col-md-1">
-
-        </div>
-        <div class="col-md-6">
-          <p class="footerBtn" onclick="navigate('Contact')"><i class="fas fa-file-signature"></i> Contact</p>
-        </div>
-        <div class="col-md-3">
-
-        </div>
-        <div class="col-md-2">
-          <p class="footerBtnStatic"> Harold de Boer</p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-1">
-
-        </div>
-        <div class="col-md-6">
-          <p class="footerBtn" onclick="navigate('About')"><i class="fas fa-address-card"></i> Over Ons</p>
-        </div>
-        <div class="col-md-3">
-
-        </div>
-        <div class="col-md-2">
-          <p class="footerBtnStatic"> Daan Smets</p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-1">
-
-        </div>
-        <div class="col-md-6">
-          <p class="footerBtn" onclick="navigate('License')"><i class="fas fa-id-badge"></i> Licenties</p>
-        </div>
-        <div class="col-md-3">
-
-        </div>
-        <div class="col-md-2">
-          <p class="footerBtnStatic"> Arend Kijk in De Vegte</p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-1">
-
-        </div>
-        <div class="col-md-6">
-          <p class="footerBtn" onclick="navigate('Privacy')"><i class="fas fa-user-secret"></i> Privacy & Data</p>
-        </div>
-        <div class="col-md-3">
-
-        </div>
-        <div class="col-md-2">
-          <p class="footerBtnStatic"> @2021 - Deltion</p>
-        </div>
-      </div>
-    </div>
-
-<!--Script JS-->
-<script src="Javascript/index.js"></script>
-</body>
-
+  </body>
 </html>
--- Webpages/login.php
+++ Webpages/login.php
@@ -1,36 +1,84 @@
 <?php include_once '../Referencepages/htmlhead.php' ?>
 <?php include_once '../Referencepages/header.php' ?>
 
-<div>
-    <p>Inloggen</p>
-    <form action="../Processpages/logintohome.php" method="post">
-        <input type="email" name="emailLogin" id="" placeholder="Emailadres">
-        <input type="password" name="passwordLogin" id="" placeholder="Wachtwoord">
-        <input type="submit" name="login" value="Inloggen">
-    </form>
-    <?php 
-        //Errors
-        if (isset($_GET["error"])) 
-        {
-            $error = $_GET["error"];
-            if ($error == "emptyField") 
-            {
-                echo '<div class="error"><p>U moet alle velden invullen!</p></div>';
-            } 
-            else if ($error == "invalidEmail") 
-            {
-                echo '<div class="error"><p>Ongeldige email!</p></div>';
-            } 
-            else if ($error == "invalidPassword") 
-            {
-                echo '<div class="error"><p>Wachtwoord verkeerd!</p></div>';
-            } 
-            else 
-            {
-                echo '<div class="error"><p>Error!</p></div>';
-            }
-        }
-    ?>
-</div>
+<!-- ? Login Box -->
+    <div id="login">
+      <div class="container">
+        <div class="row">
+          <div class="loginContainer col-md-6">
+            <div class="col-md-12">
+              <form action="../Processpages/logintohome.php" method="post">
+                <h3 class="loginText"><i class="fas fa-key"></i> Login</h3>
+                <div class="form-group">
+                  <label for="username" class="userText"
+                    ><i class="fas fa-user-alt"></i> Gebruikersnaam/email:</label
+                  ><br />
+                  <input
+                    type="text"
+                    name="emailLogin"
+                    id="username"
+                    class="form-control input"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password" class="userText"
+                    ><i class="fas fa-unlock-alt"></i> Wachtwoord:</label
+                  ><br />
+                  <input
+                    type="text"
+                    name="passwordLogin"
+                    id="password"
+                    class="form-control input"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="remember" class="userText"
+                    ><span><i class="fas fa-bookmark"></i> Onthoud Mij </span
+                    > <span
+                      ><input
+                        id="remember"
+                        name="remember"
+                        type="checkbox" /></span></label
+                  ><br /><br />
+                  <input
+                    type="submit"
+                    name="login"
+                    class="btn btn-info btn-md submitBtn"
+                    value="Inloggen"
+                  />
+                </div>
+                <div id="aanmelden" class="text-right">
+                  <a href="#" class="userText">Aanmelden</a>
+                </div>
+                <?php 
+                    //Errors
+                    if (isset($_GET["error"])) 
+                    {
+                        $error = $_GET["error"];
+                        if ($error == "emptyField") 
+                        {
+                            echo '<div class="error"><p>U moet alle velden invullen!</p></div>';
+                        } 
+                        else if ($error == "invalidEmail") 
+                        {
+                            echo '<div class="error"><p>Ongeldige email!</p></div>';
+                        } 
+                        else if ($error == "invalidPassword") 
+                        {
+                            echo '<div class="error"><p>Wachtwoord verkeerd!</p></div>';
+                        } 
+                        else 
+                        {
+                            echo '<div class="error"><p>Error!</p></div>';
+                        }
+                    }
+                ?>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 <?php include_once '../Referencepages/footer.php' ?>
